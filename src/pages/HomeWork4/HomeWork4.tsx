66c390c113a8302c601cd58884a34fe26621f0c8
fermer123 <fermer_525@mail.ru>
2023-11-27 22:21:25 +0300

    hw3

--- src/pages/HomeWork4/HomeWork4.tsx
+++ src/pages/HomeWork4/HomeWork4.tsx
@@ -1,17 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const HomeWork4 = () => {
-  const ref = useRef<HTMLButtonElement>(null);
-  console.log(ref.current);
-  useEffect(() => {
-    const btn = ref.current;
-    if (btn) {
-      btn.style.backgroundColor = "red";
-    }
+  const [color, setColor] = useState<string>("red");
+
+  const handleChangeButtonColor = useCallback(() => {
+    setColor((prev) => (prev === "red" ? "blue" : "red"));
   }, []);
+
+  useEffect(() => {
+    const btn = document.getElementById("btn");
+    if (btn) {
+      btn.addEventListener("click", handleChangeButtonColor);
+      return () => btn.removeEventListener("click", handleChangeButtonColor);
+    }
+  }, [handleChangeButtonColor]);
+
   return (
-    <div>
-      <button ref={ref}>button</button>
+    <div style={{ margin: "0 auto" }}>
+      <button
+        key="btn"
+        id="btn"
+        style={{ backgroundColor: color, width: "100px", height: "50px" }}
+      >
+        button
+      </button>
     </div>
   );
 };
