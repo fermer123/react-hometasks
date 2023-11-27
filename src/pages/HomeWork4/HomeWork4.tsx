cc0d61bef24f2f63cec18bc8b3f24671a4134b6c
fermer123 <fermer_525@mail.ru>
2023-11-27 22:24:20 +0300

    hw4

--- src/pages/HomeWork4/HomeWork4.tsx
+++ src/pages/HomeWork4/HomeWork4.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 
-export const HomeWork4 = () => {
+export const HomeWork4 = memo(() => {
   const [color, setColor] = useState<string>("red");
 
   const handleChangeButtonColor = useCallback(() => {
@@ -13,17 +13,22 @@ export const HomeWork4 = () => {
       btn.addEventListener("click", handleChangeButtonColor);
       return () => btn.removeEventListener("click", handleChangeButtonColor);
     }
-  }, [handleChangeButtonColor]);
+  }, [color]);
 
   return (
-    <div style={{ margin: "0 auto" }}>
+    <div style={{ margin: "0 auto", border: "1px solid" }}>
       <button
         key="btn"
         id="btn"
-        style={{ backgroundColor: color, width: "100px", height: "50px" }}
+        style={{
+          backgroundColor: color,
+          width: "100px",
+          height: "50px",
+          border: "1px solid",
+        }}
       >
         button
       </button>
     </div>
   );
-};
+});
